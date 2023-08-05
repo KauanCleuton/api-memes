a7fe9705d99b3228e71f846e8e2fe84d316eb00d
KauanCleuton <kauancleuton733@gmail.com>
2023-08-05 20:11:02 -0300

    New files

--- dist/index.js
+++ dist/index.js
@@ -8,6 +8,9 @@ const multer_1 = __importDefault(require("multer"));
 const cors_1 = __importDefault(require("cors"));
 const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
+const canvas_1 = require("canvas");
+const node_fs_1 = require("node:fs");
+(0, canvas_1.registerFont)('fonts/Roboto-Bold.ttf', { family: 'Roboto' });
 const storage = multer_1.default.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'uploads/');
@@ -21,20 +24,50 @@ const upload = (0, multer_1.default)({ storage: storage });
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 const memes = [];
-app.post('/memes', upload.single('image'), (request, response) => {
+app.post('/memes', upload.single('image'), async (req, res) => {
     var _a;
-    const { caption } = request.body;
-    const imagePath = (_a = request.file) === null || _a === void 0 ? void 0 : _a.path;
+    const { caption } = req.body;
+    const imagePath = (_a = req.file) === null || _a === void 0 ? void 0 : _a.path;
     if (!caption || !imagePath) {
-        return response.status(400).json({ message: 'Caption and image are required' });
+        return res.status(400).json({ message: 'Caption and image are required' });
     }
-    const newMeme = {
-        id: memes.length + 1,
-        caption,
-        imagem: imagePath,
-    };
-    memes.push(newMeme);
-    return response.status(201).json(newMeme);
+    const canvas = (0, canvas_1.createCanvas)(450, 450);
+    const ctx = canvas.getContext('2d');
+    const image = await (0, canvas_1.loadImage)(imagePath);
+    // Calcula o tamanho da imagem para não distorcê-la
+    const aspectRatio = image.width / image.height;
+    const maxWidth = 450;
+    const maxHeight = 450;
+    let width = maxWidth;
+    let height = width / aspectRatio;
+    if (height > maxHeight) {
+        height = maxHeight;
+        width = height * aspectRatio;
+    }
+    ctx.drawImage(image, 0, 0, width, height);
+    ctx.font = '30px YourCustomFont';
+    ctx.fillStyle = 'white';
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 3;
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'bottom';
+    const textX = width / 2;
+    const textY = height - 20;
+    ctx.strokeText(caption, textX, textY);
+    ctx.fillText(caption, textX, textY);
+    const outputImagePath = `${imagePath}-with-caption.png`;
+    const out = (0, node_fs_1.createWriteStream)(outputImagePath);
+    const stream = canvas.createPNGStream();
+    stream.pipe(out);
+    out.on('finish', () => {
+        const newMeme = {
+            id: memes.length + 1,
+            caption,
+            imagem: outputImagePath,
+        };
+        memes.push(newMeme);
+        res.status(201).json(newMeme);
+    });
 });
 app.get('/memes', (request, response) => {
     response.json(memes);
--- index.ts
+++ index.ts
@@ -3,6 +3,10 @@ import multer from 'multer';
 import cors from 'cors';
 import path from 'path';
 const app = express();
+import { createCanvas, loadImage, registerFont } from 'canvas';
+import { createWriteStream } from 'node:fs';
+
+registerFont('fonts/Roboto-Bold.ttf', { family: 'Roboto'});
 
 interface INewName {
   id: number;
@@ -27,24 +31,65 @@ app.use(express.json());
 
 const memes: INewName[] = [];
 
-app.post('/memes', upload.single('image'), (request: Request, response: Response) => {
-  const { caption } = request.body;
-  const imagePath = request.file?.path;
+app.post('/memes', upload.single('image'), async (req, res) => {
+  const { caption } = req.body;
+  const imagePath = req.file?.path;
 
   if (!caption || !imagePath) {
-    return response.status(400).json({ message: 'Caption and image are required' });
+    return res.status(400).json({ message: 'Caption and image are required' });
   }
 
-  const newMeme: INewName = {
-    id: memes.length + 1,
-    caption,
-    imagem: imagePath,
-  };
+  const canvas = createCanvas(450, 450);
+  const ctx = canvas.getContext('2d');
 
-  memes.push(newMeme);
+  const image = await loadImage(imagePath);
 
-  return response.status(201).json(newMeme);
+  // Calcula o tamanho da imagem para não distorcê-la
+  const aspectRatio = image.width / image.height;
+  const maxWidth = 450;
+  const maxHeight = 450;
+  let width = maxWidth;
+  let height = width / aspectRatio;
+
+  if (height > maxHeight) {
+    height = maxHeight;
+    width = height * aspectRatio;
+  }
+
+  ctx.drawImage(image, 0, 0, width, height);
+
+  ctx.font = '30px YourCustomFont'; 
+  ctx.fillStyle = 'white';
+  ctx.strokeStyle = 'black'; 
+  ctx.lineWidth = 3; 
+  ctx.textAlign = 'center';
+  ctx.textBaseline = 'bottom'; 
+  const textX = width / 2;
+  const textY = height - 20; 
+
+
+  ctx.strokeText(caption, textX, textY);
+  ctx.fillText(caption, textX, textY);
+
+  const outputImagePath = `${imagePath}-with-caption.png`;
+  const out = createWriteStream(outputImagePath);
+  const stream = canvas.createPNGStream();
+  stream.pipe(out);
+
+  out.on('finish', () => {
+    const newMeme: INewName = {
+      id: memes.length + 1,
+      caption,
+      imagem: outputImagePath,
+    };
+
+    memes.push(newMeme);
+
+    res.status(201).json(newMeme);
+  });
 });
+
+
 
 app.get('/memes', (request: Request, response: Response) => {
   response.json(memes);
